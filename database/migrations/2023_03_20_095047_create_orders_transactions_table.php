350cc27abaf7770119ec13aa3acd560e64252d1e
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-07-17 13:44:20 +0700

    fix: change data type from string to text

--- database/migrations/2023_03_20_095047_create_orders_transactions_table.php
+++ database/migrations/2023_03_20_095047_create_orders_transactions_table.php
@@ -18,7 +18,7 @@ class CreateOrdersTransactionsTable extends Migration
             $table->unsignedBigInteger('order_id');
             $table->unsignedBigInteger('payment_method_id');
             $table->bigInteger('amount');
-            $table->string('description');
+            $table->text('description');
             $table->date('date');
             $table->timestamps();
         });
