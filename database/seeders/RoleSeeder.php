665e5d0cfc8359e0385a72b7381fd621ccc5c6dc
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-07-19 12:37:17 +0700

    fix: add created_at and updated_at

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -18,58 +18,86 @@ class RoleSeeder extends Seeder
             [
                 "user_id" => 1,
                 "name" => "dashboard",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "master-jenis-pembayaran",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "master-kategori",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "data-customer",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "data-barang",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "penjualan",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "lihat-penjualan",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "uang-kas",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "data-users",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "laporan-uang-kas",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "tracking",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "orders",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "order-transaction",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 "user_id" => 1,
                 "name" => "order-tracking",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]
         ]);
     }
