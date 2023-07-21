3932afedaee7773aa52597efc508a6cede68c75e
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-07-21 19:58:11 +0700

    fix: enable RoleSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        $this->call(RoleSeeder::class);
         $this->call(PaymentMethodSeeder::class);
         $this->call(PrintTypeSeeder::class);
     }
